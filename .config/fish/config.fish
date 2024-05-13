87cb756bfef789a39e59235b457e9be9d340eedf
SenseiSamy <samy.naji2003@gmail.com>
2024-05-13 17:31:29 +0200

    dfhdfh

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,17 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    alias lock="/sgoinfre/goinfre/Perso/jmaia/Public/pimp_my_lock/pimp_my_lock.sh /mnt/nfs/homes/snaji/Videos/JokerVictoryScreen.mp4 0 0 1440 2560 && clear"
 end
 
+alias lock="/sgoinfre/goinfre/Perso/jmaia/Public/pimp_my_lock/pimp_my_lock.sh /mnt/nfs/homes/snaji/Videos/JokerVictoryScreen.mp4 0 0 1440 2560 && clear"
+
+alias valg_all="valgrind --leak-check=full --track-fds=yes --trace-children=yes"
+
+alias valg_l="valgrind --leak-check=full"
+
+alias clipboard="xclip -selection clipboard"
+
 alias vim nvim
+
+alias nvim nvim.appimage
 
 starship init fish | source
