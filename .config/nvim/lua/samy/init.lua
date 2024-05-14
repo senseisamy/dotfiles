b60fa6796a64cfef3d3d9926607ddd393bcca1f9
SenseiSamy <samy.naji2003@gmail.com>
2024-05-14 18:35:10 +0200

    dfhdfhdh

--- .config/nvim/lua/samy/init.lua
+++ .config/nvim/lua/samy/init.lua
@@ -3,3 +3,10 @@ require("samy.lazy")
 require("samy.treesitter")
 require("samy.lsp")
 require("samy.telescope")
+
+local set = vim.opt
+
+set.relativenumber = true
+set.tabstop = 4
+set.shiftwidth = 4
+set.autoindent = true
